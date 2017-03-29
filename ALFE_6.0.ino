574d815c1044e9c2888f75a6ebcc6beeab3f5c3d
ConnerCollie <connercollieaitr@gmail.com>
2017-03-29 07:42:18 -0400

    Add files via upload

---
+++ ALFE_6.0.ino
@@ -0,0 +1,109 @@
+#include <Servo.h>
+Servo Lmotor;    //Makes L and R Servo
+Servo Rmotor;
+const int joypiny = A0;    //attaches x and y to pin A1 and A2
+const int joypinx = A4;
+float Xaxis = 0;
+float Yaxis = 0;
+float axisY = 0;
+float axisX = 0;
+
+
+
+void setup() {
+   Serial.begin(9600);
+   Lmotor.attach(5);
+   Rmotor.attach(3);
+   pinMode(joypinx, INPUT);
+   pinMode(joypiny, INPUT);
+}
+
+void loop() {
+  
+  Xaxis = analogRead(joypinx);
+  Yaxis = analogRead(joypiny);
+  axisX = map(Xaxis, 0, 1023, 0, 180);
+  axisY = map(Yaxis, 0, 1023, 0, 180);
+  
+
+  //pause DEAD ZONE
+  if (axisX < 100 && axisX > 80 && axisY < 100 && axisY > 80)
+  {
+    Rmotor.write(95);
+    Lmotor.write(95);
+  }
+
+ 
+  else if (axisX > 100 && axisY < 115 && axisY > 70)
+  {
+    Lmotor.write(112.2);
+    Rmotor.write(78);         //add one to make even motors
+    Serial.print("FORWARD ");
+  }  
+
+   
+  else if (axisX < 80 && axisY < 110 && axisY > 70)
+  {
+    Lmotor.write(80.2);                          
+    Rmotor.write(112);
+    Serial.print("BACKWARD ");
+  }
+ 
+  else if (axisX > 100 && axisY > 100)
+  {
+    Lmotor.write(114);
+    Rmotor.write(78.2);
+    Serial.print("Upper right ");
+  }
+
+  else if (axisX > 100 && axisY < 80)
+  {
+    Lmotor.write(112);
+    Rmotor.write(76.2);
+    Serial.print("Upper left ");
+  }
+
+  else if (axisX > 80 & axisX < 100 && axisY < 80)
+  {
+    Lmotor.write(83);
+    Rmotor.write(83);  
+    Serial.print("LEFT ");
+  }
+  
+  
+  else if (axisX > 80 & axisX < 100 && axisY > 100)
+  {
+    Lmotor.write(107);
+    Rmotor.write(107);
+    Serial.print("RIGHT ");   
+  }
+  
+  else if (axisX < 80 && axisY < 80)
+  {
+    Lmotor.write(78.8);
+    Rmotor.write(115);
+    Serial.print("Lower Left ");
+   }
+
+  
+  else if (axisX < 80 && axisY > 100)
+  {
+    Lmotor.write(75.8);
+    Rmotor.write(112);
+    Serial.print("Lower Right "); 
+  }
+
+  
+  analogRead(joypinx);   //monitor joystick values
+  Serial.print("X ");
+  Serial.print(Xaxis);
+  Serial.print("/ ");
+  Serial.print(axisX);
+  analogRead(joypiny);
+  Serial.print(" Y ");
+  Serial.print(Yaxis);
+  Serial.print("/ ");
+  Serial.println(axisY);
+
+}
+
---
+++ ALFE_TankDrive_1.0.ino
@@ -0,0 +1,105 @@
+#include <Servo.h>
+#define joypinX A0   //attaches joysticks to pins
+#define joypinX1 A1
+#define KillBpin 8
+#define SpeedBpin 12
+Servo Lmotor;    //declares that they are motors/servo
+Servo Rmotor;
+float Xaxis = 0;   
+float X1axis = 0;    //sets all to zero and declares them 
+float axisX1 = 0;    //X1axis blue
+float axisX = 0;
+float KillB = 0;
+float SpeedB = 0;
+
+void setup() {
+   Serial.begin(9600);
+   Lmotor.attach(5);     //motor Pins
+   Rmotor.attach(3);
+   pinMode(joypinX, INPUT);  //sets joysticks to read
+   pinMode(joypinX1, INPUT);
+   pinMode(KillBpin, INPUT);  //sets joysticks to read
+   pinMode(SpeedBpin, INPUT);
+}
+
+void loop() {
+  digitalRead(KillBpin);
+  if (KillBpin == 1){delay(750); KillB++;}
+
+  if (KillB == 1){
+    digitalRead(SpeedBpin);
+    if (KillBpin == 1){delay(450); KillB--;}
+    if (SpeedB == 0){
+      if (SpeedBpin == 1){delay(750); SpeedB++;}
+      Xaxis = analogRead(joypinX);    //joypin = pin plug in
+      X1axis = analogRead(joypinX1);
+      axisX = map(Xaxis, 0, 1023, 0, 180);  //Xaxis 0-1023
+      axisX1 = map(X1axis, 0, 1023, 78, 112); //axisX 0-180
+
+      //pause DEAD ZONE
+      if (axisX < 100 && axisX > 80 && axisX1 < 100 && axisX1 > 80)
+      {
+        Rmotor.write(95);
+        Lmotor.write(95);
+      }
+    
+   
+      else if (axisX > 100 && axisX1 < 115 && axisX1 > 70)
+      {
+        Lmotor.write(axisX);
+        Rmotor.write(axisX1);         //add one to make even motors
+        Serial.print("Movement ");
+      }  
+  
+   
+    
+      analogRead(joypinX);   //monitor joystick values
+      Serial.print("X ");
+      Serial.print(Xaxis);
+      Serial.print("/ ");
+      Serial.print(axisX);
+      analogRead(joypinX1);
+      Serial.print("  1X ");
+      Serial.print(X1axis);
+      Serial.print("/ ");
+      Serial.println(axisX1);
+    }
+    if (SpeedB == 1){
+      if (KillBpin == 1){KillB--;}
+      if (SpeedBpin == 1){SpeedB--;}
+      Xaxis = analogRead(joypinX);    //joypin = pin plug in
+      X1axis = analogRead(joypinX1);
+      axisX = map(Xaxis, 0, 1023, 0, 180);  //Xaxis 0-1023
+      axisX1 = map(X1axis, 0, 1023, 60, 130); //axisX 0-180
+
+      //pause DEAD ZONE
+      if (axisX < 100 && axisX > 80 && axisX1 < 100 && axisX1 > 80)
+      {
+        Rmotor.write(95);
+        Lmotor.write(95);
+      }
+    
+   
+      else if (axisX > 100 && axisX1 < 115 && axisX1 > 70)
+      {
+        Lmotor.write(axisX);
+        Rmotor.write(axisX1);         //add one to make even motors
+        Serial.print("Movement ");
+      }  
+  
+   
+    
+      analogRead(joypinX);   //monitor joystick values
+      Serial.print("X ");
+      Serial.print(Xaxis);
+      Serial.print("/ ");
+      Serial.print(axisX);
+      analogRead(joypinX1);
+      Serial.print("  1X ");
+      Serial.print(X1axis);
+      Serial.print("/ ");
+      Serial.println(axisX1);
+    }
+  }
+}
+
